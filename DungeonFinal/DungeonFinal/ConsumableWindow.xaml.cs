eb37c858e0ba43112edf8a8d9e318b4b15f64ebe
jake-flynn <jakedflynn11@yahoo.com>
2015-05-27 14:13:09 -0700

    created consumable window. also set up template for adding image brush method to items

---
+++ DungeonFinal/DungeonFinal/ConsumableWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DungeonFinal
+{
+    /// <summary>
+    /// Interaction logic for ConsumableWindow.xaml
+    /// </summary>
+    public partial class ConsumableWindow : Window
+    {
+        public ConsumableWindow()
+        {
+            InitializeComponent();
+        }
+
+        //start methods
+        public void updateVisuals()
+        {
+            
+        }
+
+
+        //end methods
+
+        private void btn_item1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item11_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item14_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item15_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item16_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item17_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item18_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item19_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_item20_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- DungeonFinal/DungeonFinal/Item.cs
+++ DungeonFinal/DungeonFinal/Item.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace DungeonFinal
 {
@@ -139,5 +147,9 @@ namespace DungeonFinal
         {
             return sockets.Length;
         }
+
+
+        //abstract methods
+        public abstract ImageBrush getBrush();
     }
 }
