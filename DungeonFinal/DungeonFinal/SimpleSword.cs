1df10e7336a32a37b2294e348940b70fdfd96466
Tanner Race <tannerrace@eagles.ewu.edu>
2015-05-27 15:02:09 -0700

    Created simple weapons for the heroes. Axe, Sword, Dagger and Staff.

---
+++ DungeonFinal/DungeonFinal/SimpleSword.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DungeonFinal
+{
+    class SimpleSword : Item
+    {
+        ItemsEffect _effect;
+
+        public SimpleSword()
+        {
+            _effect = new ItemsEffect();
+            this.setItemName("Simple Sword");
+            this.setEquippable(true);
+            _effect.setEffectName("A simple sword");
+            _effect.setStrengthValue(15);
+            _effect.setEffectAmount(15);
+            this.setEffect(_effect);
+        }
+
+        public override ImageBrush getBrush()
+        {
+            ImageBrush imgBrush = new ImageBrush();
+            BitmapImage image = new BitmapImage(new Uri(@"https://lh3.googleusercontent.com/-VE9mF5_fgt4/VV7rvGU76XI/AAAAAAAAA3o/HIMeg3vKuaM/w506-h354/Hellhound.jpg"));
+            imgBrush.ImageSource = image;
+            return imgBrush;
+        }
+    }
+}
