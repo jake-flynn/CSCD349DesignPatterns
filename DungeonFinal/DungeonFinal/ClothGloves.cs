92766c904d8c8022e359264175d7202eb39b9aa3
Tanner Race <tannerrace@eagles.ewu.edu>
2015-05-27 23:47:47 -0700

    I have created the cloth classes for the cleric class. I am currently working on the chain mail class for the swordsman. I have decided that the paladin should wear bronze armor based upon what his picture looks like. I can then create different tiers of armor classes for them.

---
+++ DungeonFinal/DungeonFinal/ClothGloves.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DungeonFinal
+{
+    class ClothGloves : Item
+    {
+        ItemsEffect _effect;
+
+        public ClothGloves()
+        {
+            _effect = new ItemsEffect();
+            this.setItemName("Cloth Gloves");
+            this.setEquippable(true);
+            _effect.setEffectName("Cloth gloves that increase mana slightly");
+            _effect.setPhysicalDefense(2);
+            _effect.setManaValue(2);
+            _effect.setEffectAmount(2);
+            this.setEffect(_effect);
+        }
+
+        public override ImageBrush getBrush()
+        {
+            ImageBrush imgBrush = new ImageBrush();
+            BitmapImage image = new BitmapImage(new Uri(@"https://lh3.googleusercontent.com/-VE9mF5_fgt4/VV7rvGU76XI/AAAAAAAAA3o/HIMeg3vKuaM/w506-h354/Hellhound.jpg"));
+            imgBrush.ImageSource = image;
+            return imgBrush;
+        }
+    }
+}
