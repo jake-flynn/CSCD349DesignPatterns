9bb14332b5c4a4a59893d0c5c83dc64a29d81254
Tanner Race <tannerrace@eagles.ewu.edu>
2015-06-03 11:34:53 -0700

    Working on new armor.

---
+++ DungeonFinal/DungeonFinal/SteelHelmet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DungeonFinal
+{
+    class SteelHelmet : Equipment
+    {
+        ItemsEffect _effect;
+
+        public SteelHelmet() : base()
+        {
+            _effect = new ItemsEffect();
+            this.setItemName("Steel Helmet");
+            this.setIsHelmet(true);
+            this.setSocketAmount(3);
+            _effect.setEffectName("A well crafted helmet made of steel, slightly increases health");
+            _effect.setPhysicalDefense(12);
+            _effect.setResistanceDefense(12);
+            _effect.setHealthValue(3);
+            _effect.setEffectAmount(12);
+            this.setEffect(_effect);
+
+            ImageBrush imgBrush = new ImageBrush();
+            BitmapImage image = new BitmapImage(new Uri(@"../../Images/Items/bronzehelmet.jpg", UriKind.RelativeOrAbsolute));
+            imgBrush.ImageSource = image;
+            setImageBrush(imgBrush);
+        }
+    }
+}
